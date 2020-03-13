30f6805df3403882dac8e0a5fb73088d9160158a
Trogious <trog@swmud.net>
2020-03-13 12:12:22 +0000

    params and args, and cleanup

--- expand.py
+++ expand.py
@@ -1,34 +1,57 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import glob, sys, re
+import argparse
+import glob
+import re
+import sys
 
 reGroup = re.compile('{[^{}]*}', re.I)
 
+
 def expandBraces(entry):
-	if entry is None:
-		return None
-	expandedEntries = []
-	print('e ' + entry)
-	match = reGroup.search(entry)
-	if match is not None:
-		group = match.group()
-		items = group[1:-1].split(',')
-		for item in items:
-			#print('i: ' + item)
-			expanded = entry[:match.start()] + item + entry[match.end():]
-			#print('e: ' + expanded)
-			expandedEntries = expandedEntries + expandBraces(expanded)
-	else:
-		if entry in expandedEntries:
-			expandedEntries = []
-		else:
-			expandedEntries = [entry]
-	return expandedEntries
+    if entry is None:
+        return None
+    expandedEntries = []
+    match = reGroup.search(entry)
+    if match is not None:
+        group = match.group()
+        items = group[1:-1].split(',')
+        for item in items:
+            expanded = entry[:match.start()] + item + entry[match.end():]
+            expandedEntries = expandedEntries + expandBraces(expanded)
+    else:
+        if entry in expandedEntries:
+            expandedEntries = []
+        else:
+            expandedEntries = [entry]
+    return expandedEntries
 
-if len(sys.argv) > 1:
-	for arg in sys.argv[1:]:
-		for expanded in expandBraces(arg):
-			print('expanded: ' + str(expanded))
-			for g in glob.glob(expanded):
-				print(g)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog=sys.argv[0])
+    parser.add_argument('-s', '--single-line', action='store_true')
+    parser.add_argument('-q', '--single-quotes', action='store_true')
+    parser.add_argument('-d', '--double-quotes', action='store_true')
+    parser.add_argument('-D', '--delimiter', default=' ')
+    parser.add_argument('pattern', nargs='*')
+    args = parser.parse_args(sys.argv[1:])
+    if args.single_quotes:
+        quot = "'"
+    elif args.double_quotes:
+        quot = '"'
+    else:
+        quot = ''
+    if args.single_line:
+        print_out = sys.stdout.write
+    else:
+        print_out = print
+    for arg in args.pattern:
+        i = 0
+        for expanded in expandBraces(arg):
+            for g in glob.glob(expanded):
+                if args.single_line and i > 0:
+                    sys.stdout.write(args.delimiter)
+                print_out(quot + g + quot)
+                i += 1
+    if args.single_line:
+        print()
